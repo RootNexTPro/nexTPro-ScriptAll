c1f350edfceca2b763922ea365eb7a8af58770ef
RootNexTPro <n.aomierachel031@googlemail.com>
2026-05-08 16:34:24 +0100

    Mise à jour pour remplacer l'ancien repo avec RootNexTPro/nexTPro-ScriptAll

--- autoinstall.sh
+++ autoinstall.sh
@@ -14,9 +14,10 @@ echo "precedence ::ffff:0:0/96  100" >> /etc/gai.conf
 sysctl -w net.ipv6.conf.all.disable_ipv6=1 >/dev/null 2>&1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1 >/dev/null 2>&1
 
-# 3. Téléchargement du Lanceur Principal depuis TON laboratoire
+# 3. Téléchargement du Lanceur Principal depuis RootNexTPro
+SERVER_HOST="https://raw.githubusercontent.com/RootNexTPro/nexTPro-ScriptAll/main"
 echo "[+] Connexion au dépôt autonome Nexus..."
-wget -qO /root/nexus.sh "https://raw.githubusercontent.com/naomierachel031-lab/Clone-script-all-buddy/main/nexus.sh"
+wget -qO /root/nexus.sh "$SERVER_HOST/nexus.sh"
 
 # 4. Exécution Sécurisée
 if [ -f /root/nexus.sh ]; then
